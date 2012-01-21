a7e4670a3281e9cd5b45ef63c683a73ef0616848
Ulrich Green <ugreen@gmail.com>
2012-01-21 17:33:53 +0100

    Adds various changes to the wordpress admin

--- tester.php
+++ tester.php
@@ -39,11 +39,7 @@ class wpWings {
 		if($shots) {
 		}
 	}
-	// CUSTOM ADMIN MENU LINK FOR ALL SETTINGS
-   function all_settings_link() {
-    add_options_page(__('All Settings'), __('All Settings'), 'administrator', 'options.php');
-   }
-   add_action('admin_menu', 'all_settings_link');
+
    
    // CUSTOM ADMIN LOGIN HEADER LOGO
    function my_custom_login_logo() {
--- wings.php
+++ wings.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Test plugin
+Plugin Name: WordPress Wings
 Plugin URI: http://wpwings.com/
 Version: 0.1
 Author: Ulrich Green
@@ -20,8 +20,18 @@ class Wings {
 
 		// Set Plugin URL
 		$this->pluginUrl = WP_PLUGIN_URL . '/wings';
+		
+		// Turn on features
+		add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
+		
 		add_action('login_head', array($this, 'login_head'));
 		add_action('admin_menu', array($this, 'admin_menu'));
+		add_action('wp_dashboard_setup', array($this, 'wp_dashboard_setup'));
+		
+		add_filter('admin_footer_text', array($this, 'admin_footer_text'));
+		add_filter('mce_buttons_3', array($this, 'add_more_buttons'));
+		
+		add_shortcode('code', array($this, 'bbcode'));
 	}
 
 	// CUSTOM ADMIN MENU LINK FOR ALL SETTINGS
@@ -30,12 +40,40 @@ class Wings {
 	}
 	
 	function login_head() { 
-		?><style type="text/css">
-			.login h1 a {
-				background: url(<?php echo $this->pluginUrl.'/images/logo.png'; ?>) no-repeat top center;
-				margin-bottom: 10px;
-			}
-		</style><?php
+		echo '<style type="text/css">';
+		echo '	.login h1 a {';
+		echo '		background: url('.$this->pluginUrl.'/images/login-logo.png) no-repeat top center;';
+		echo '		margin-bottom: 10px;';
+		echo '	}';
+		echo '</style>';
+	}
+	function add_more_buttons($buttons) {
+		$buttons[] = 'hr';
+		$buttons[] = 'del';
+		$buttons[] = 'sub';
+		$buttons[] = 'sup';
+		$buttons[] = 'fontselect';
+		$buttons[] = 'fontsizeselect';
+		$buttons[] = 'cleanup';
+		$buttons[] = 'styleselect';
+		$buttons[] = 'wp_page';
+		return $buttons;
+	}
+	function bbcode( $attr, $content = null ) {
+		$content = clean_pre($content); // Clean pre-tags
+		return '<pre"><code>' .
+				str_replace('<', '<', $content) . // Escape < chars
+				'</code></pre>';
+	}
+	function wings_dashboard_widget() {
+		echo '<h4>WordPress Wings Dashboard Widget</h4>';
+		echo '<p>Some content.</p>';
+	}
+	function wp_dashboard_setup() {
+		wp_add_dashboard_widget('dashboard_wings', __( 'WordPress Wings' ), array($this, 'wings_dashboard_widget'));
+	}
+	function admin_footer_text () {
+		echo 'Awesome! This WordPress site has <a href="http://wpwings.com">Wings</a>';
 	}
 }
 new Wings;
